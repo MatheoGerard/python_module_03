5b9801ce734aeab9efe19eb73cdb44fda307e4a5
Matheo Gerard <mgerard@fu-r4-p7.s19.be>
2026-05-30 15:29:44 +0200

    add color

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -5,7 +5,7 @@ def score_catsing(score: str, score_valid: list[int]) -> None:
     try:
         score_valid.append(int(score))
     except ValueError:
-        print(f"Invalid parameter: '{score}'")
+        print(f"\033[31mInvalid parameter: '{score}'\033[0m")
 
 
 def parsing_score(scores: list[str]) -> None:
@@ -14,15 +14,15 @@ def parsing_score(scores: list[str]) -> None:
         score_catsing(args, score_valid)
     if len(score_valid) <= 0:
         print(
-            "No scores provided. Usage: python3 "
-            "ft_score_analytics.py <score1> <score2> ..."
+            "\033[31mNo scores provided. Usage: python3 "
+            "ft_score_analytics.py <score1> <score2> ...\033[0m"
         )
         return
-    print(f"Scores processed: {score_valid}")
+    print(f"\033[35mScores processed: {score_valid}\033[0m")
     print(f"Total players: {len(score_valid)}")
     print(f"Total score: {sum(score_valid)}")
     print(f"Average score: {float(sum(score_valid) / len(score_valid))}")
-    print(f"High score: {max(score_valid)}")
+    print(f"\033[33mHigh score: {max(score_valid)}\033[0m")
     print(f"Low score: {min(score_valid)}")
     print(f"Range score: {max(score_valid) - min(score_valid)}")
 
--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,5 +1,22 @@
-def get_player_pos():
+import math
+
+
+def casting_input(str_input: str) -> None:
+    try:
+
+
+
+
+def get_player_pos() -> None:
     print("Get a first set of coordinates")
+    is_good_pos = False
+    while not is_good_pos:
+        user_input: list[str] = input(
+            "Enter new coordinates as floats in format 'x,y,z': "
+        ).split()
+        for _ in user_input:
+            try:
+
 
 
 if __name__ == "__main__":
